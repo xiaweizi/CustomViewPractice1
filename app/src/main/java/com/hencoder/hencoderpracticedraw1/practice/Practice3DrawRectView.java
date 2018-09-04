fc2c927500347e831d0ebe8f924d0dd7d726aadf
xiaweizi <1012126908@qq.com>
2018-09-04 20:44:12 +0800

    [dev] 自定义 View rect

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice3DrawRectView.java
@@ -2,11 +2,16 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice3DrawRectView extends View {
+
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
 
     public Practice3DrawRectView(Context context) {
         super(context);
@@ -14,6 +19,7 @@ public class Practice3DrawRectView extends View {
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -21,9 +27,22 @@ public class Practice3DrawRectView extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+        float rectWidth = mWidth / 3;
+        float rectHeight = mHeight / 2;
+        canvas.drawRect(mWidth / 2 - rectWidth / 2,
+                mHeight / 2 - rectHeight /2,
+                mWidth / 2 + rectWidth / 2,
+                mHeight / 2 + rectHeight /2, mPaint);
     }
 }
