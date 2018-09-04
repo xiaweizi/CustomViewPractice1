6be8616024516534681520f9fb016d0db5734e49
xiaweizi <1012126908@qq.com>
2018-09-04 20:40:34 +0800

    [dev] 自定义 View Circle

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,11 +2,17 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -14,10 +20,18 @@ public class Practice2DrawCircleView extends View {
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
@@ -26,5 +40,23 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        float centerX = mWidth / 3;
+        float centerY = mHeight / 3;
+        float radius = mHeight / 6;
+        // 1、绘制实心圆
+        canvas.drawCircle(centerX, centerY, radius, mPaint);
+        // 2、绘制空心圆
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+        canvas.drawCircle(centerX * 2, centerY, radius, mPaint);
+        // 3、绘制蓝色空心圆
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(centerX, centerY * 2, radius, mPaint);
+        //4、绘制线宽 20 的空心圆
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(20);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawCircle(centerX * 2, centerY * 2, radius, mPaint);
     }
 }
