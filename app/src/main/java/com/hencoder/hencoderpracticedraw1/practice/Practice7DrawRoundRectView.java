4c96d71bcc1825dd8a438afb39188ed1e0c608db
xiaweizi <1012126908@qq.com>
2018-09-04 21:54:50 +0800

    [dev] 自定义 View roundRect

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice7DrawRoundRectView.java
@@ -2,11 +2,17 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice7DrawRoundRectView extends View {
+    private Paint mPaint;
+    private int mWidth;
+    private int mHeight;
+    private RectF rectF;
 
     public Practice7DrawRoundRectView(Context context) {
         super(context);
@@ -14,6 +20,8 @@ public class Practice7DrawRoundRectView extends View {
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rectF = new RectF();
     }
 
     public Practice7DrawRoundRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -21,9 +29,23 @@ public class Practice7DrawRoundRectView extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        float rectWidth = mWidth / 2;
+        float rectHeight = mHeight / 2;
+        rectF.set(mWidth / 2 - rectWidth / 2,
+                mHeight / 2 - rectHeight / 2,
+                mWidth / 2 + rectWidth / 2,
+                mHeight / 2 + rectHeight / 2);
+        canvas.drawRoundRect( rectF, 50, 50,  mPaint);
     }
 }
